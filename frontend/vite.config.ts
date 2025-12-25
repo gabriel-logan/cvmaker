a7717e8767f63c044219657b17e8a6bd8f7763be
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-12-25 11:09:19 -0300

    fix: remove envDir configuration to streamline Vite setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { join } from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  envDir: join(__dirname, "..", ".."),
   plugins: [
     tailwindcss(),
     react({
